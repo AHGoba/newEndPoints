b7aae9cd643fbe70ee495dc92575a5a6fcac64b6
ahmed.hamdy.50117 <ahmed.hamdy.50117@EgyptGas.com>
2025-04-28 15:31:18 +0300

    28/4/2025 get districtsContracts

--- AssetsManagementEG/AssetsManagementEG.Presentation/Controllers/ContractCarController.cs
+++ AssetsManagementEG/AssetsManagementEG.Presentation/Controllers/ContractCarController.cs
@@ -1,9 +1,11 @@
 ﻿using AssetsManagementEG.DTOs.CompanyL;
 using AssetsManagementEG.DTOs.ContractCar;
 using AssetsManagementEG.Models.Models;
+using AssetsManagementEG.Repositories.Many_ManyRepo;
 using AssetsManagementEG.Repositories.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AssetsManagementEG.Presentation.Controllers
@@ -14,11 +16,14 @@ namespace AssetsManagementEG.Presentation.Controllers
     {
         ContractCarRepository ContractCarRepository;
         DistrictRepository DistrictRepository;
+        MDistrictCarRepo mDistrictCarRepo;
 
-        public ContractCarController(ContractCarRepository _ContractCarRepository, DistrictRepository _districtRepository)
+        public ContractCarController(ContractCarRepository _ContractCarRepository
+            , DistrictRepository _districtRepository , MDistrictCarRepo _mDistrictCarRepo)
         {
             ContractCarRepository = _ContractCarRepository;
             DistrictRepository = _districtRepository;
+            mDistrictCarRepo = _mDistrictCarRepo;
         }
 
 
@@ -104,6 +109,31 @@ namespace AssetsManagementEG.Presentation.Controllers
             return Ok("Contract was successfully added");
         }
 
+        [HttpPost("GetContractsForDistricts")]
+        public IActionResult GetContractsForDistricts([FromBody] List<int> districtIds)
+        {
+            if (districtIds == null || !districtIds.Any())
+            {
+                return BadRequest("Please provide at least one district ID.");
+            }
+
+            var contracts = ContractCarRepository.GetContractsByDistricts(districtIds);
+
+            if (contracts == null || !contracts.Any())
+            {
+                return NotFound("لم يتم العثور على عقود مرتبطة بالمناطق المحددة.");
+            }
+
+            var result = contracts.Select(c => new
+            {
+                ContractId = c.ContractId,
+                ContractName = c.ContractName,
+                IsAvailable = c.IsAvailable,
+                CarContractorsName = c.CarContractors != null ? c.CarContractors.Name : null
+            }).ToList();
+
+            return Ok(result);
+        }
 
     }
 }
--- AssetsManagementEG/AssetsManagementEG.Repositories/Repositories/ContractCarRepository.cs
+++ AssetsManagementEG/AssetsManagementEG.Repositories/Repositories/ContractCarRepository.cs
@@ -30,5 +30,40 @@ namespace AssetsManagementEG.Repositories.Repositories
         {
             return context.Contract.Where(c => c.DistrictId == id).ToList();
         }
+
+        // for Super User
+        public List<Contract> GetContractsByDistricts(List<int> districtIds)
+        {
+            // Step 1: Get all CarIds related to these Districts
+            var carIds = context.DistrictCar
+                .Where(dc => districtIds.Contains(dc.DistrictId))
+                .Select(dc => dc.CarId)
+                .Distinct()
+                .ToList();
+
+            if (!carIds.Any())
+            {
+                return new List<Contract>(); // رجع List فاضية لو مفيش عربيات
+            }
+
+            // Step 2: Get all Contract IDs related to these Cars
+            var contractIds = context.ContractsCars
+                .Where(cc => carIds.Contains(cc.CarId))
+                .Select(cc => cc.ContractId)
+                .Distinct()
+                .ToList();
+
+            if (!contractIds.Any())
+            {
+                return new List<Contract>(); // رجع List فاضية لو مفيش عقود
+            }
+
+            // Step 3: Get the contracts
+            var contracts = context.Contract
+                .Where(c => contractIds.Contains(c.ContractId))
+                .ToList();
+
+            return contracts;
+        }
     }
 }
